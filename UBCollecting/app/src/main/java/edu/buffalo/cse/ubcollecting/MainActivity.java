f3595e9061c71966a139c8b98aaabeda516e9839
Aamel Unia <aameluni@buffalo.edu>
2018-02-17 11:40:13 -0500

    Decided to create database from within android studio instead of forming external connection

--- UBCollecting/app/src/main/java/edu/buffalo/cse/ubcollecting/MainActivity.java
+++ UBCollecting/app/src/main/java/edu/buffalo/cse/ubcollecting/MainActivity.java
@@ -2,12 +2,19 @@ package edu.buffalo.cse.ubcollecting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import edu.buffalo.cse.ubcollecting.dbcode.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Database Helper
+    DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = new DatabaseHelper(getApplicationContext());
+        
     }
 }
